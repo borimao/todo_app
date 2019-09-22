34f7d66dda217c11a82da38496cdc5b8a579bb51
EC2 Default User <ec2-user@ip-172-31-23-198.us-east-2.compute.internal>
2019-09-22 12:15:26 +0000

    Make a basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   resources :tasks
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190922071722) do
+ActiveRecord::Schema.define(version: 20190922120934) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
